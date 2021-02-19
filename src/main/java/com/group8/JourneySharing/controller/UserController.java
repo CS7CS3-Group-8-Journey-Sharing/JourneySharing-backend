ebfe52766e49c8ebcf686dd2ee0bc8e01eb3c7ea
christielise <jan.jime.serra@gmail.com>
2021-02-19 10:29:53 +0100

    fix getMapping instead of postMapping on getAllUsers and getUser

--- src/main/java/com/group8/JourneySharing/controller/UserController.java
+++ src/main/java/com/group8/JourneySharing/controller/UserController.java
@@ -6,10 +6,8 @@ import com.group8.JourneySharing.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 
 @RestController
@@ -24,12 +22,12 @@ public class UserController {
         return new ResponseEntity<>(userService.addUser(new User(username)), HttpStatus.CREATED);
     }
 
-    @PostMapping(value = "/getuser")
+    @GetMapping(value = "/getuser")
     public ResponseEntity<User> getUserByUsername(String username) {
         return new ResponseEntity<>(userService.getUserByUsername(username), HttpStatus.OK);
     }
 
-    @PostMapping(value = "/getallusers")
+    @GetMapping(value = "/getallusers")
     public ResponseEntity<ArrayList> getUsers() {
         return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
