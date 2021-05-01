6ec988298c7ecd4153ba6450ee9de8e4e82a5159
Jan Jiménez Serra <jan.jime.serra@gmail.com>
2021-05-01 12:05:50 +0200

    Add find journey within radius
    Rest API endpoint example:
    
    api/journeysharing/journey/getjourneys?lat=53.344921&lng=-6.256449&radius=500

--- src/main/java/com/group8/JourneySharing/service/JourneyService.java
+++ src/main/java/com/group8/JourneySharing/service/JourneyService.java
@@ -3,10 +3,14 @@ package com.group8.JourneySharing.service;
 import com.group8.JourneySharing.entity.Journey;
 import com.group8.JourneySharing.vo.NewJourneyVo;
 
+import java.util.List;
+
 public interface JourneyService {
 
 
     Journey createJourney(NewJourneyVo newJourney);
 
     Journey getJourneyByID(String journeyID);
+
+    List<Journey> getJourneysWithinRadius(double lat, double lng, int radius);
 }
